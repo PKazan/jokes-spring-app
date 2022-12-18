b0dcecbccd63cb1d20e122b6f8939bafb468446d
Pawel Kazanecki <pawel.kazanecki@capgemini.com>
2022-12-18 22:50:57 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/jokesspringapp/JokesSpringAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokesspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/jokesspringapp/JokesSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokesspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
